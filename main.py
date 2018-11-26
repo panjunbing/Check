9b2e0c464177a352a1b856ee9aaf2db7c154b3c5
panpan <531826568@qq.com>
2018-11-26 22:59:22 +0800

    修复了读取excel表格的一些bug

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ if __name__ == "__main__":
     # 主机列表
     host_list = []
 
+    data_list = []
+
     # 批量读取主机列表
     workbook = xlrd.open_workbook("host.xls")
     sheet = workbook.sheet_by_index(0)
     for i in range(sheet.nrows-1):
-        ip = sheet.cell(i+1, 0)
-        port = sheet.cell(i+1, 1)
-        username = sheet.cell(i+1, 2)
-        password = sheet.cell(i+1, 3)
-        cmd = sheet.cell(i+1, 4)
-        re = sheet.cell(i+1, 5)
+        ip = sheet.cell(i+1, 0).value.encode('utf-8')
+        port = int(sheet.cell(i+1, 1).value)
+        username = sheet.cell(i+1, 2).value.encode('utf-8')
+        password = ""
+        if sheet.cell(i+1, 3).ctype == 1:
+            password = sheet.cell(i+1, 3).value
+        elif sheet.cell(i+1, 3).ctype == 2:
+            password = str(int(sheet.cell(i + 1, 3).value))
+        cmd = sheet.cell(i+1, 4).value.encode('utf-8')
+        re = sheet.cell(i+1, 5).value.encode('utf-8')
         host = Host(ip, port, username, password, cmd, None)
         host_list.append(host)
 
@@ -26,6 +32,7 @@ if __name__ == "__main__":
     for i in range(len(host_list)):
         conn = SSHConnection(host)
         conn.connect()
-        data = conn.exec_command(host.cmd)
+        data_list.append(conn.exec_command(host.cmd))
         conn.close()
-        print data
+
+    print data_list
